720649bad60a3870e93a6a87d2b07b8a5cd4010f
etelletxea <e.tellechea@edu.gva.es>
2024-03-08 14:20:01 +0100

    Código para ejercicios de POO básico cambios en Main Pintura

--- src/excepciones/PeligroException.java
+++ src/excepciones/PeligroException.java
@@ -3,10 +3,10 @@ package excepciones;
 public class PeligroException extends Exception{
     private final ErrorCode code;
 
-    public PeligroException(ErrorCode code) {
+    public PeligroException(int code) {
         this.code = code;
     }
-    public void showMessage(int bienes,Equipaje equipaje){
+    public void showMessage(){
         System.out.println("código de error: "+code.INVALID_INPUT+" el objeto "+bienes+" del equipaje "+ equipaje.nombre + "pertenece a sustancias peligrosas");
     }
 }
